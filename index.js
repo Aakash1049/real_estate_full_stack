4c75990fe909878f6c1ad98347e3dbae669a25c9
Aakash Sangapure <aakashdadasangapure@gmail.com>
2023-01-03 11:36:00 +0530

    a

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ const property = require("./routes/property")
 app.use(property);
 
 
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
+
+
 app.listen(process.env.PORT || 8000, () => {
     console.log("Server running on port 8000");
 });
